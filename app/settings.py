bca74c1ac17705d743da615fb3a8aec0a1eb5e62
Aline <alislima2013@gmail.com>
2024-08-22 13:40:51 -0300

    db

--- app/settings.py
+++ app/settings.py
@@ -13,7 +13,7 @@ import dj_database_url
 from dotenv import load_dotenv
 import os
 from pathlib import Path
-
+load_dotenv() 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,9 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ("DEBUG", "False").lower() == "true"
+DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
 
 
 # Application definition
@@ -85,9 +86,13 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+DATABASE_URL = os.environ.get("DATABASE_URL", "")
+if DATABASE_URL:
+    DATABASES["default"] = dj_database_url.parse(DATABASE_URL)
+else:
+    raise ValueError("A variável de ambiente DATABASE_URL não está definida.")
 
-database_url = os.environ.get("DATABASES_URL")
-DATABASES["default"] = dj_database_url.parse("database_url")
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
--- build.sh
+++
@@ -1,4 +0,0 @@
-python3 pip install -r requirements.txt
-
-python3 manage.py collectstatic --no-input
-python3 manage.py migrate
