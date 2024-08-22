440be67fea1bb9645bf052550436bc44383b90d9
Aline <alislima2013@gmail.com>
2024-08-22 13:49:03 -0300

    db2

--- app/settings.py
+++ app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', "projeto-django-generation-app.onrender.com"]
 
 
 
